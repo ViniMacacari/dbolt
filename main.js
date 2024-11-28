ae0fc55a68c57b000fe1ee7de87d061853e4cdfd
ViniMacacari <macacarivinicius@gmail.com>
2024-11-28 00:57:37 -0300

    Integrating electron with angular

---
+++ main.js
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+let win
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+
+    const angularIndexPath = path.join(process.resourcesPath, 'static/browser/index.html')
+    win.loadFile(angularIndexPath)
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
