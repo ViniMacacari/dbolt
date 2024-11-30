5f121dc2357d4226c7c802fbf0cbd6b4a632fe5a
ViniMacacari <macacarivinicius@gmail.com>
2024-11-30 09:24:01 -0300

    Sidebar and code space

--- src/app/pages/database-manager/database-manager.component.ts
+++ src/app/pages/database-manager/database-manager.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
+import { SidebarComponent } from "../../components/sidebar/sidebar.component"
 
 @Component({
   selector: 'app-database-manager',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './database-manager.component.html',
   styleUrl: './database-manager.component.scss'
 })
