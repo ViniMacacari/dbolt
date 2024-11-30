27e767afaf68131c7e9d0681ebb9a9684a9c237c
ViniMacacari <macacarivinicius@gmail.com>
2024-11-30 09:41:12 -0300

    Tab styles

--- src/app/pages/database-manager/database-manager.component.ts
+++ src/app/pages/database-manager/database-manager.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { SidebarComponent } from "../../components/sidebar/sidebar.component"
+import { TabsComponent } from "../../components/tabs/tabs.component"
 
 @Component({
   selector: 'app-database-manager',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, TabsComponent],
   templateUrl: './database-manager.component.html',
   styleUrl: './database-manager.component.scss'
 })
