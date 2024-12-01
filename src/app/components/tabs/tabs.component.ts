9176b0d312fde7bbe8597f83b3ad4060bfec02e4
ViniMacacari <macacarivinicius@gmail.com>
2024-12-01 16:19:33 -0300

    Loading

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Router, ActivatedRoute } from '@angular/router'
+import { InputListComponent } from "../elements/input-list/input-list.component"
+import { InternalApiService } from '../../services/requests/internal-api.service'
 
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [],
+  imports: [InputListComponent],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss'
 })
 export class TabsComponent {
+  dataList: any = []
 
+  constructor(
+    private route: ActivatedRoute,
+    private IAPI: InternalApiService
+  ) { }
+
+  async ngAfterViewInit(): Promise<void> {
+    const routeParams = this.route.snapshot.paramMap
+    const routeParamId = Number(routeParams.get('id'))
+    const database = await this.IAPI.get(`/api/connections/${routeParamId}`)
+    
+  }
 }
