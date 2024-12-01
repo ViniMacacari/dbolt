3dc591990da849dc599d976d9a8885c6515da46c
ViniMacacari <macacarivinicius@gmail.com>
2024-12-01 15:42:26 -0300

    Searching databases and schemas

--- api/services/lists/postgres/v9.js
+++ api/services/lists/postgres/v9.js
@@ -3,6 +3,7 @@ import PgV1 from '../../../models/postgres/v9.js'
 class LSPg1 {
     constructor() {
         this.db = new PgV1()
+        this.mainConfig = null
     }
 
     async listDatabasesAndSchemas() {
@@ -14,6 +15,16 @@ class LSPg1 {
         }
 
         try {
+            if (!this.mainConfig) {
+                this.mainConfig = {
+                    host: this.db.connection.host,
+                    port: this.db.connection.port,
+                    user: this.db.connection.user,
+                    password: this.db.connection.password,
+                    database: this.db.connection.database
+                }
+            }
+
             const databasesQuery = `
                 SELECT datname AS database_name 
                 FROM pg_database 
@@ -22,22 +33,38 @@ class LSPg1 {
             const databases = await this.db.executeQuery(databasesQuery)
 
             const results = []
+
             for (const dbInfo of databases) {
                 const { database_name } = dbInfo
+
+                const tempDb = new PgV1()
+                await tempDb.connect({
+                    host: this.mainConfig.host,
+                    port: this.mainConfig.port,
+                    user: this.mainConfig.user,
+                    password: this.mainConfig.password,
+                    database: database_name
+                })
+
                 const schemaQuery = `
                     SELECT schema_name 
                     FROM information_schema.schemata 
-                    WHERE catalog_name = $1
                 `
-                const schemas = await this.db.executeQuery(schemaQuery, [database_name])
+                const schemas = await tempDb.executeQuery(schemaQuery)
+
                 results.push({
                     database: database_name,
                     schemas: schemas.map(schema => schema.schema_name)
                 })
+
+                await tempDb.disconnect()
             }
+
+            await this.db.connect(this.mainConfig)
 
             return { success: true, data: results }
         } catch (error) {
+            console.error('Error in listDatabasesAndSchemas:', error)
             return {
                 success: false,
                 message: 'Error occurred while listing databases and schemas.',
