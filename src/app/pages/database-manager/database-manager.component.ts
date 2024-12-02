2f2072eb671886da818ad822c5bd56faad7fa2d7
ViniMacacari <macacarivinicius@gmail.com.br>
2024-12-02 09:26:14 -0300

    Listing all databases and schemas

--- src/app/pages/database-manager/database-manager.component.ts
+++ src/app/pages/database-manager/database-manager.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core'
+import { InternalApiService } from '../../services/requests/internal-api.service'
+import { Router, ActivatedRoute } from '@angular/router'
 import { SidebarComponent } from "../../components/sidebar/sidebar.component"
 import { TabsComponent } from "../../components/tabs/tabs.component"
+import { LoadingComponent } from '../../components/modal/loading/loading.component'
 
 @Component({
   selector: 'app-database-manager',
@@ -10,5 +13,44 @@ import { TabsComponent } from "../../components/tabs/tabs.component"
   styleUrl: './database-manager.component.scss'
 })
 export class DatabaseManagerComponent {
+  activeConnection: any = {}
+  databasesSchemasActiveConnections: any[] = []
+  connections: any[] = []
 
+  constructor(
+    private IAPI: InternalApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  async ngAfterViewInit(): Promise<void> {
+    LoadingComponent.show()
+    await this.firstConnectionConfig()
+    await this.pageConnectionConfig()
+  }
+
+  getPageId() {
+    const routeParams = this.route.snapshot.paramMap
+    const routeParamId = Number(routeParams.get('id'))
+    return routeParamId
+  }
+
+  async firstConnectionConfig(): Promise<void> {
+    try {
+      this.activeConnection = await this.IAPI.get('/api/connections/' + this.getPageId())
+      console.log(this.activeConnection)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async pageConnectionConfig(): Promise<void> {
+    try {
+      this.connections = await this.IAPI.get('/api/connections/load')
+      this.databasesSchemasActiveConnections = await this.IAPI.get(`/api/${this.activeConnection.database}/${this.activeConnection.version}/list-databases-and-schemas`)
+      console.log(this.connections, this.databasesSchemasActiveConnections)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }
