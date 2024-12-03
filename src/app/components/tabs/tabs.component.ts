64a79c0b189a8a52f8bd2a5c9a58b9c1c81bd90f
ViniMacacari <macacarivinicius@gmail.com.br>
2024-12-03 14:16:54 -0300

    App: Add a new request options

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, HostListener } from '@angular/core'
+import { CommonModule } from '@angular/common'
 import { Router, ActivatedRoute } from '@angular/router'
 import { InputListComponent } from "../elements/input-list/input-list.component"
 import { InternalApiService } from '../../services/requests/internal-api.service'
@@ -6,12 +7,13 @@ import { InternalApiService } from '../../services/requests/internal-api.service
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss'
 })
 export class TabsComponent {
   dataList: any = []
+  dropdownVisible: boolean = false
 
   constructor(
     private route: ActivatedRoute,
@@ -22,5 +24,27 @@ export class TabsComponent {
     const routeParams = this.route.snapshot.paramMap
     const routeParamId = Number(routeParams.get('id'))
     const database = await this.IAPI.get(`/api/connections/${routeParamId}`)
+  }
+
+  toggleDropdown(): void {
+    this.dropdownVisible = !this.dropdownVisible
+  }
+
+  newQuery(): void {
+    console.log('Opção 1 selecionada')
+    this.dropdownVisible = false
+  }
+
+  loadQuery(): void {
+    console.log('Opção 2 selecionada')
+    this.dropdownVisible = false
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent): void {
+    const targetElement = event.target as HTMLElement
+    if (!targetElement.closest('.add')) {
+      this.dropdownVisible = false
+    }
   }
 }
