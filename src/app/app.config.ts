b80026f584f0e9276ec342002dc0e0806e01dbd7
ViniMacacari <macacarivinicius@gmail.com>
2024-12-07 14:43:38 -0300

    App: Build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,16 @@ import { provideHttpClient } from '@angular/common/http'
 
 import { routes } from './app.routes'
 
+(window as any).MonacoEnvironment = {
+  getWorkerUrl: function (moduleId: string, label: string) {
+    if (label === 'json') return './assets/monaco-editor/json.worker.js'
+    if (label === 'css') return './assets/monaco-editor/css.worker.js'
+    if (label === 'html') return './assets/monaco-editor/html.worker.js'
+    if (label === 'typescript' || label === 'javascript') return './assets/monaco-editor/ts.worker.js'
+    return './assets/monaco-editor/editor.worker.js'
+  }
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
