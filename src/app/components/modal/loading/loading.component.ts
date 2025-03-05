45427630e552a30f4381cf3f19f6a98faee26a83
ViniMacacari <macacarivinicius@gmail.com.br>
2025-03-05 14:08:25 -0300

    app - feat: display elapsed time in loading modal

--- src/app/components/modal/loading/loading.component.ts
+++ src/app/components/modal/loading/loading.component.ts
@@ -11,19 +11,42 @@ import { CommonModule } from '@angular/common'
 export class LoadingComponent {
   static instance: LoadingComponent
   isLoading = false
-  @Input() message: string = 'Connecting to database...'
+  seconds = 0
+  minutes = 0
+  static interval: any = null
+  static startTime = 0
+  @Input() message = 'Connecting to database...'
 
   constructor(private renderer: Renderer2) {
     LoadingComponent.instance = this
   }
 
   static show() {
+    if (LoadingComponent.interval) {
+      clearInterval(LoadingComponent.interval)
+    }
+
     LoadingComponent.instance.isLoading = true
+    LoadingComponent.instance.seconds = 0
+    LoadingComponent.instance.minutes = 0
     LoadingComponent.instance.renderer.setStyle(document.body, 'overflow', 'hidden')
+    LoadingComponent.startTime = Date.now()
+
+    LoadingComponent.interval = setInterval(() => {
+      const elapsedTime = Math.floor((Date.now() - LoadingComponent.startTime) / 1000)
+      LoadingComponent.instance.minutes = Math.floor(elapsedTime / 60)
+      LoadingComponent.instance.seconds = elapsedTime % 60
+    }, 1000)
   }
 
   static hide() {
+    if (LoadingComponent.interval) {
+      clearInterval(LoadingComponent.interval)
+      LoadingComponent.interval = null
+    }
     LoadingComponent.instance.isLoading = false
+    LoadingComponent.instance.seconds = 0
+    LoadingComponent.instance.minutes = 0
     LoadingComponent.instance.renderer.removeStyle(document.body, 'overflow')
   }
 }
