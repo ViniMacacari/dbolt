89fb9f702730aa6ec797d5c2c17bfe850c96ce59
ViniMacacari <macacarivinicius@gmail.com>
2026-07-21 10:41:58 -0300

    feat: add schema and object diagram endpoints to CSQLiteV3 and CSQLServerV1

--- api/router/sqlite/v3.ts
+++ api/router/sqlite/v3.ts
@@ -43,6 +43,12 @@ router.get('/table-indexes/:tableName', (req, res) => {
 router.get('/table-ddl/:tableName', (req, res) => {
   void CSQLiteV3.tableDDL(req, res);
 });
+router.get('/diagram/schema', (req, res) => {
+  void CSQLiteV3.schemaDiagram(req, res);
+});
+router.get('/diagram/object/:tableName', (req, res) => {
+  void CSQLiteV3.objectDiagram(req, res);
+});
 router.get('/procedure-ddl/:procedureName', (req, res) => {
   void CSQLiteV3.procedureDDL(req, res);
 });
