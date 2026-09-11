682c38f7caf0c4b9b821dc4efae0b66145e5e6e8
ViniMacacari <macacarivinicius@gmail.com>
2026-09-11 08:31:57 -0300

    feat: add HttpClient provider to LoadQueryComponent tests for improved HTTP handling

--- src/app/components/modal/load-query/load-query.component.spec.ts
+++ src/app/components/modal/load-query/load-query.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { LoadQueryComponent } from './load-query.component';
 
@@ -8,7 +9,8 @@ describe('LoadQueryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoadQueryComponent]
+      imports: [LoadQueryComponent],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
 
